25b803056ca6e15eb1d59526f71aa45bc9164b8f
Turbo1263 <45638439+Turbo1263@users.noreply.github.com>
2025-03-04 17:11:12 -0500

    Init

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+struct SysMem {
+    memory: [u8; 4096],
+    memory_pointer: u32,
+}
+
+impl SysMem {
+
+    fn new() -> SysMem {
+        SysMem {
+            memory: [0; 4096], //PrePopulate with Font later
+            memory_pointer: 0
+        }
+    }
+
+    fn hex_string_to_int(hex_string: &str) -> Option<u64> {
+        // Attempt to parse the hexadecimal string into a u64 integer
+        match u64::from_str_radix(hex_string, 16) {
+            // If parsing succeeds, return the parsed integer wrapped in Some
+            Ok(parsed_int) => Some(parsed_int),
+            // If parsing fails (invalid input), return None
+            Err(_) => None,
+        }
+    }
+}
+
+fn main() {
+    let memory = SysMem::new();
+    let number = SysMem::hex_string_to_int("F");
+    let mut newnumber: u64 = 5;
+    if number.is_some() {
+        newnumber = number.unwrap();
+    }
+    println!("{}", newnumber.to_string());
+}
